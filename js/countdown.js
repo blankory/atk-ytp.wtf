9b78c2fddd5fcfd4c2974891b28cc0f30887cbe5
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2020-08-19 18:56:37 +0300

    Init commit

---
+++ js/countdown.js
@@ -0,0 +1,16 @@
+var countDownDate = new Date("Oct 21, 2020 10:15:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Nyt kyllä suruttaa.";
+  }
+}, 1000);
