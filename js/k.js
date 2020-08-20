aa42727c7652bb8701a93bad39a00a65436ce477
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2020-08-20 16:29:11 +0300

    Important content

---
+++ js/k.js
@@ -0,0 +1,18 @@
+function onKonamiCode(cb) {
+  var input = '';
+  var key = '38384040373937396665';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    if (input === key) {
+      return cb();
+    }
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	onKonamiCode(function () {document.body.classList.add("k");})
+});
+
+
