6c7648077b68f0c1d5d0f310d19ffc0ff2da66b3
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2020-08-21 16:07:54 +0300

    Fix ä in np

--- js/k.js
+++ js/k.js
@@ -12,14 +12,14 @@ function vimmiaHuikka(cb) {
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-	var np = "SmFub3MgVmFsbXVuZW4gLSBCdXNzaXB5c8Oka2lsbMOkIChCbG9vZCBSYXZlIEJvbWJha2xhYXQgUmVtaXgp";
+	var np = "SmFub3MgVmFsbXVuZW4gLSBCdXNzaXB5cyZhdW1sO2tpbGwmYXVtbDsgKEJsb29kIFJhdmUgQm9tYmFrbGFhdCBSZW1peCk";
 	vimmiaHuikka(function () {
 		document.body.classList.add("k");
 		var a = new Audio(atob("bXVzaWMvazEubXAz"), 1.5*1337/1*3*4.1337%80, true);
 		a.loop = true;
 		a.play();
 		npt=document.createElement('p');
-		npt.innerText=atob("Tm93IHBsYXlpbmc6IA")+atob(np);
+		npt.innerText = atob("Tm93IHBsYXlpbmc6IA")+atob(np).replace(/&auml;/g,"ä");
 		var c=document.getElementById("countdown");
 		c.parentNode.insertBefore(npt,c.nextSibling);
 
