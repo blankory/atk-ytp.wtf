418324cbd2f2fae6f9b55b37880436aa83e4a4a0
Niklas Riekki <niklas.riekki@gmail.com>
2020-08-21 13:26:01 +0300

    fixed date on countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 21, 2020 10:15:00").getTime();
+var countDownDate = new Date("Oct 21, 2020 10:15:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
