a386191cb5178e1c5aa7c9e22c0dbbfd65b54b7f
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2020-08-24 18:29:31 +0300

    2021 init

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 21, 2020 10:15:00").getTime();
+var countDownDate = new Date("Oct 21, 2021 10:15:00").getTime();
 
 var x = setInterval(function() {
   var now = new Date().getTime();
@@ -7,7 +7,7 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = "~" + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
   if (distance < 0) {
     clearInterval(x);
