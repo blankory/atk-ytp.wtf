ab88b4255e46ed4f220585f1b30c930a7421085a
Juhani Karppinen <juhani.k.karppinen@gmail.com>
2021-06-27 07:26:17 +0300

    Add scss support to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-
+const gulp = require('gulp');
 const { src, dest, watch, task, series } = require('gulp');
 const pug = require('gulp-pug');
+var sass = require('gulp-sass')(require('sass'));
 
 task('watch', function(){
    watch('./*.pug', series('pugToHtml'));
+   watch('./scss/**/*.scss', series('styles'));
 });
 
 task('pugToHtml', function(){
@@ -12,15 +14,23 @@ task('pugToHtml', function(){
          //options
       }))
       .pipe(dest('./dist'));
-  });
+});
 
-  task('build', function(){
-   return src('./*.pug')
+task('build', async function(){
+   gulp.src('./*.pug')
    .pipe(pug({
          //options
       }))
       .pipe(dest('./dist'));
-  });
+   generateCss();
+});
 
+/* SCSS */ 
+async function generateCss(){
+    gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css/'));
+}
 
+gulp.task('styles', generateCss);
 
